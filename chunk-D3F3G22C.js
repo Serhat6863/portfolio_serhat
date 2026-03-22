fa03d07c6d8ce4a42c369158916932869ce00367
Serhat6863 <serhatkurklu@icloud.com>
2026-03-22 16:49:07 +0100

    Auto-generated commit

--- chunk-D3F3G22C.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-LONTFYQ5.js";import"./chunk-67HI2W2E.js";import"./chunk-GWGWIYPE.js";import"./chunk-YLJ26XQO.js";import"./chunk-XBJPK2I2.js";export{a as Projects};
--- chunk-G37IKKLF.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-ZB2Q27PT.js";import"./chunk-YLJ26XQO.js";import"./chunk-XBJPK2I2.js";export{a as Skills};
